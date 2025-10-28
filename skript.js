16797ecad5d16dcfba71603e1a56fb749bef4420
Olga <olga.machulnaya@gmail.com>
2025-10-28 13:20:51 +0300

    Задание 9

--- skript.js
+++ skript.js
@@ -1,17 +1,8 @@
 
-let a = 9;
-let b = 2;
-let quotient =a%b;
-alert(quotient);
 
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num += 1;
-num -= 1;
-alert(num);
-
-
+const user {
+    name:"Шарик",
+    age:"1",
+    isAdmin:true,
+}
  
