e394dcd333b95d1b163b19787f28fd4bddddd25e
Olga <olga.machulnaya@gmail.com>
2025-10-28 12:12:21 +0300

    Задание 4

--- skript.js
+++ skript.js
@@ -1,5 +1,18 @@
-let a = 20;
-alert(a);
+
+let a = 10;
+let b = 2;
+let sum = a + b;
+let difference =a - b;
+let product = a*b;
+let quotient =a/b;
+alert(sum,);
+alert(difference);
+alert(product)
+alert(quotient);
+
+
+
+
  let age = ('Первый iPhone был выпущен в 2007 году.');
  alert (age);
  let name = ('Брендан Айх');
