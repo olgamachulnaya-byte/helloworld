c6797fe95c015c1dba80d1fff91ba5aae1916a18
Olga <olga.machulnaya@gmail.com>
2025-10-29 10:14:38 +0300

    Добавила задания

--- skript.js
+++ skript.js
@@ -1,3 +1,52 @@
+let a = 20;
+alert(a);
+
+let b = 10;
+alert(b);
+
+let age = ('Первый iPhone был выпущен в 2007 году.');
+alert (age);
+ 
+let name = ('Брендан Айх');
+alert (name);
+
+let a = 10;
+let b = 2;
+let sum = a + b;
+let difference =a - b;
+let product = a*b;
+let quotient =a/b;
+alert(sum,);
+alert(difference);
+alert(product)
+alert(quotient);
+
+let b = 2;
+let result = b**5;
+alert(result);
+
+let c = 9;
+let d = 2;
+let quotient =a%b;
+alert(quotient);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+const user {
+    name:"Шарик",
+    age:"1",
+    isAdmin:true,
+}
 
 let name = prompt('Как вас зовут?');
 alert(`Привет, ${name}!`);
