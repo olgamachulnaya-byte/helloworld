fc08e6605286860ffb469a1fc99ba34691591ab2
Olga <olga.machulnaya@gmail.com>
2025-10-29 12:18:38 +0300

    Дз 3

--- skript.js
+++ skript.js
@@ -4,11 +4,11 @@ alert(a);
 let b = 10;
 alert(b);
 
-let age = ('Первый iPhone был выпущен в 2007 году.');
-alert (age);
+let iPhone = ('Первый iPhone был выпущен в 2007 году.');
+alert (iPhone);
  
-let name = ('Брендан Айх');
-alert (name);
+let im = ('Брендан Айх');
+alert (im);
 
 let a = 10;
 let b = 2;
@@ -45,7 +45,7 @@ alert(age);
 
 const user {
     name:"Шарик",
-    age:"1",
+    age:"2",
     isAdmin:true,
 };
 
@@ -87,4 +87,48 @@ if(c > 0 && c < 10){
     console.log('Верно');
 }else{
     console.log('Неверно');
+}
+
+
+let m = 80;
+let e = 120;
+
+if(m > 100 || e > 100) {
+    console.log('Верно'); 
+} else {
+    console.log('Неверно');}
+
+
+let y = '2'; 
+let u = '3'; 
+alert(parseInt(u) + parseInt(y));    
+
+const monthNumber = 12; 
+if(monthNumber > 12 || monthNumber <= 0) {
+    console.log("Недопустимый номер месяца.");
+} else {    
+    switch(monthNumber) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Зима");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Весна");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Лето");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("Осень");
+            break;
+        default:
+            console.log("Ошибка!");
+    }
 }
