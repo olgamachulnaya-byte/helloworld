cf6d1612fc3106b7f4ebfe7b2d92c96e629cb121
Olga <olga.machulnaya@gmail.com>
2025-11-01 10:32:14 +0300

    Домашняя работа

--- skript.js
+++ skript.js
@@ -78,3 +78,44 @@ if(monthNumber > 12 || monthNumber <= 0) {
             console.log("Ошибка!");
     }
 }
+
+
+console.log("Привет");
+console.log("Привет");
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+for (let i = 7; i <= 22; i++) {
+  console.log(i);
+}
+
+const obj = {
+  "Коля": '200',
+  "Вася": '300',
+  "Петя": '400'
+};
+
+for (let key in obj) {
+  console.log(`${key} — зарплата ${obj[key]} долларов.`);
+}
+
+
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+  n = n / 2;
+  num++;
+}
+
+console.log("Результат:", n);
+console.log("Количество итераций:", num);
+
+const firstFriday = 3; // например, первая пятница — 3-е число месяца
+const daysInMonth = 31;
+
+for (let day = firstFriday; day <= daysInMonth; day += 7) {
+  console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+}
