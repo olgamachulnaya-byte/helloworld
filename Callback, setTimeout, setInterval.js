934a318ea6a88caa2156c208e8ba6a004d6b228f
Olga <olga.machulnaya@gmail.com>
2025-11-29 10:09:46 +0300

    Callback, setTimeout, setInterval

---
+++ Callback, setTimeout, setInterval.js
@@ -0,0 +1,72 @@
+//Задание 1
+const people = [
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
+
+console.log(people.sort((a, b) => a.age - b.age));
+//Задание2
+function isPositive(number) {
+    return number > 0;
+}
+
+function isMale(person) {
+    return person.gender === 'male';
+}
+
+function filter(array, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive)); // [3, 1, 9]
+
+const people1 = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(people1, isMale)); 
+
+// Задание3
+console.log('Запуск программы...');
+
+const intervalId = setInterval(() => {
+    const now = new Date();
+    console.log(now.toLocaleString());
+}, 3000);
+
+setTimeout(() => {
+    clearInterval(intervalId);
+    console.log('30 секунд прошло');
+}, 30000);
+
+//Задание 4
+function delayForSecond(callback) {
+    setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+   console.log('Привет, Глеб!');
+});
+//Задание 5
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+function sayHi (name) {
+    console.log('Привет, ${name}!');
+}
+delayForSecond(() => sayHi('Глеб'));
+
